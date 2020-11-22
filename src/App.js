9fb2329b65c9d726a010cbdbae6a58e2c2c4eb36
MOBUMIN <kiju23@naver.com>
2020-11-22 22:35:50 +0900

    1-6 complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
+import Wrapper from './Wrapper.js';
 
+{/* 주석 엄청 불편하게 다는군 */}
 function App() {
+  const name = 'helloworld';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize:24,
+    padding: '1rem'
+  }
   return (
-    <div>
+    <Wrapper>
+      <Hello name="이것은프롭스" color="red" isSpecial />
       <Hello />
-      <Hello />
-      <Hello />
-    </div>
+      <div style = {style}>{name}</div>
+      <div className="gray-box"></div>
+    </Wrapper>
   );
 }
 
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function Hello() {
-  return <div>헬로월드</div>
+function Hello({color, name, isSpecial}) {
+  return <div style={{ color }}>
+      { isSpecial && <b>*</b>}
+      헬로월드 {name}
+      </div>
+}
+
+Hello.defaultProps={
+    name: '노프롭스'
 }
 
 export default Hello;
