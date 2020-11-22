f412fd2d87d4a179ae6d72ed7fe72d6952729122
MOBUMIN <kiju23@naver.com>
2020-11-22 22:30:27 +0900

    1-6 complete

---
+++ src/Wrapper.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Wrapper({children}) {
+  const style = {
+    border: '2px solid black',
+    padding:16,
+    margin:16,
+  };
+  return (
+    <div style={style}>
+        {children}
+    </div>
+  )
+}
+
+export default Wrapper;
